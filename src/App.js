7379c5c829cd097f5a6b7478b2e3f749a5bba7c5
坂本大五郎 <skmtdgr@daigoro.local>
2021-01-15 06:55:44 +0900

    implement component using multiple state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import React, { useState } from 'react';
-import { render } from 'react-dom';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
-
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-  const real = () => setCount((previousCount) => previousCount * 2);
-  const real3 = () => setCount((previousCount) => {
-
-    return previousCount % 3 === 0 ? previousCount / 3 : previousCount
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
     
-    })
-  
+  };
   return (
     <React.Fragment>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={real}>×２</button>
-        <button onClick={real3}>3</button>
-        <button onClick={reset}>Reset</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </React.Fragment>
   );
 };
 
+App.defaultProps = {
+  name: '',
+  price: 1000,
+};
 
 export default App;
