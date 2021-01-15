d79edd3c71caa207b4b14d6a2c6a9a4fda70cf2a
坂本大五郎 <skmtdgr@daigoro.local>
2021-01-16 07:30:52 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const {name, price} = state
+
+  useEffect(() => {
+    console.log('useEffect is aaa')
+    
+  },[])
+  useEffect(() => {
+    console.log('useEffect is bbb')
+    
+  },[name])
+
+ 
+
 
   return (
     <React.Fragment>
