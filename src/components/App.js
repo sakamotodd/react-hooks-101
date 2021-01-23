3845cb64532197c8852e90ba1d44a0550ff8adfc
坂本大五郎 <skmtdgr@daigoro.local>
2021-01-23 13:21:00 +0900

    setup useContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
 import EventForm from './EventForm';
-import React, {useReducer} from 'react';
-
+import React, { useReducer} from 'react';
+import AppContext from '../contexts/AppContext';
 import Events from './Events';
+
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}/>
-      <Events state={state} dispatch={dispatch}/>
-   
-    </div>
+    <AppContext.Provider value={'I am wwwwww'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 export default App;
