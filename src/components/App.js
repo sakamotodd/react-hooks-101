c0132d2cb7688cdbeb7f7e728c711168c1a76cee
坂本大五郎 <skmtdgr@daigoro.local>
2021-01-23 13:51:04 +0900

    solve prope drilling issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
 import EventForm from './EventForm';
-import React, { useReducer} from 'react';
+import React, { useReducer } from 'react';
 import AppContext from '../contexts/AppContext';
 import Events from './Events';
-
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <AppContext.Provider value={'I am wwwwww'}>
+    <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
-        <EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   );
