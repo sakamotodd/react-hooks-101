d147d7a11aa2a3c413693990c08a5a72e07332ce
坂本大五郎 <skmtdgr@daigoro.local>
2021-01-24 13:07:29 +0900

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,35 @@
+import React, { useReducer, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
 import EventForm from './EventForm';
-import React, { useReducer } from 'react';
 import AppContext from '../contexts/AppContext';
 import Events from './Events';
 import OperationLogs from './OperationLogs';
-// import operationLogs from '../reducers/operationLogs';
+
+const APP_KEY = 'appWithRedux'
 
 const App = () => {
-  const initialState = {
-    events:[],
-    operationLogs:[]
-  }
+  const appState = localStorage.getItem(APP_KEY);
+  //parse＝文字列化したappStateをJavaScriptに還元するメソッド
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
   const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    //JSON.stringifyは文字列を返す
+
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
         <EventForm />
         <Events />
-        <OperationLogs/>   
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
